502a92ccee5d97c2fe2293590aedfc3dd15bf6b9
Gracie Richards <gracier@gmail.com>
2024-07-30 22:05:03 -0400

    Adding trees to navigation scene

--- Assets/Scripts/UnitySetupInstructions.cs
+++ Assets/Scripts/UnitySetupInstructions.cs
@@ -5,8 +5,20 @@ Give the GameObject for the opponent the tag "Enemy".
 Create an empty GameObject called BattleManager. Add the BattleManager script to this object.
 Drag the GameObjects for the player and the opponent to their respective fields in the BattleManager script.
 
-Drag the EmotionSystem script onto both the GameObjects for Circe and the opponent. Attach the temp objects for attacks to both scripts, 
-in the prefabs folder
+Drag the EmotionSystem script onto both the GameObjects for Circe and the opponent. Attach the Circe Spell Cloud and Enemy Spell Cloud 
+prefabs to both scripts
 
 Drag the VisualController script onto the BattleManager GameObject. Add the objects
+
+Drag each of the "arrow selected" Game Objects under Canvas to the appropriate fields in the Visual Controllers in Circe, the opponent, and the Battle Manager
+
+Drag the "Strum" audio to the SFX fields in each Visual Controller
+
+Set the slope limit for Circe's character controller to 60
+Set move speed in the Third Person Controller script to 2.
+
+Credits:
+Potion images from an asset pack by Josué Rocha
+Sound effects by Dustyroom
+Additional tree assets from a pack by past12pm
 */
