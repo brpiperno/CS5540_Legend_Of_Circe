dab46719846deb841209991ce3ea104e4cf6b96c
Gracie Richards <gracier@gmail.com>
2024-08-13 15:15:53 -0400

    minor edit

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public static int numEnemiesDefeated = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void BattleWon() {
+
+    }
+
+    public void BattleLost() {
         
     }
 }
