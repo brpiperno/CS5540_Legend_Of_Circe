43b16096281a7015f86629b29ff6cdb7f11f6a40
Gracie Richards <gracier@gmail.com>
2024-08-16 11:57:22 -0400

    Set all base powers to 20, commented out gizmos, changed getchild by index to by name in MenuHandlerNavigation

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Rendering;
+//using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
